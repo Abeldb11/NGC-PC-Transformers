d84f76974700fba6b8792bc26c17d8d7eb38c371
Abel Berhanu <berhanuabel79@gmail.com>
2026-07-31 01:33:49 +0300

    y_mu

--- model.py
+++ model.py
@@ -593,7 +593,7 @@ class NGCTransformer:
 
         # ══════  Learning  ═════════════════════════════════════════
         EFE = 0.            ## expected free energy
-        y_mu = jnp.zeros((self.batch_size * self.seq_len, self.vocab_size))  ## settled prediction
+        y_mu =0  ## settled prediction
         if adapt_synapses:
             ## ════════════════════════════════
             ## Perform several E-steps
