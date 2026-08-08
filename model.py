fd65e9b77c3521c974d769dc745a5df926ca0642
Abeldb11 <avilala@Avilala.localdomain>
2026-08-08 12:15:39 +0300

    using tanh activation for output

--- model.py
+++ model.py
@@ -595,7 +595,7 @@ class NGCTransformer:
 
         self.output.E_out.weights.set(jnp.transpose(self.output.W_out.weights.get()))
         current_out = self.output.z_out.z.get()
-        projected_out = self.projection.q_out_Ratecell.z.get()
+        projected_out = self.projection.q_out_Ratecell.zF.get()
         self.output.z_out.z.set((1. - alpha) * current_out + alpha * projected_out)
         # self.output.e_out.dmu.set(self.projection.eq_target.dmu.get())
         # self.output.e_out.dtarget.set(self.projection.eq_target.dtarget.get())
