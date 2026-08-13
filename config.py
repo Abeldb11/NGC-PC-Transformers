059b021331ef485e96337874e48e5f220b7ddd6f
Abeldb11 <avilala@Avilala.localdomain>
2026-08-13 18:08:50 +0300

    using wikitext103

--- config.py
+++ config.py
@@ -41,4 +41,4 @@ class Config:
     # set True to Use jax.lax.scan fused advance loop (faster, minor floating-point differences from the normal python loop)
     fused_advance = True
     # "tinyshakespeare", "wikitext2", "wikitext103"
-    dataset = "wikitext2"
+    dataset = "wikitext103"
